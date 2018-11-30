64e543208cc8c6648b09ff1a7eef6589e1a60516
Julia Nedialkova <julianedialkova@hotmail.com>
2018-11-30 11:28:51 +0200

    WIP
    [#159728114]
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- scripts/deploy-lite-rootless.sh
+++ scripts/deploy-lite-rootless.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 manifests=$(cd "$(dirname "$0")/../manifests" && pwd)
 "$(dirname "$0")"/deploy-lite.sh \
+  "$@" \
   -o "$manifests/bpm.ops.yml" \
   -o "$manifests/noop-networking.ops.yml" \
   -o "$manifests/rootless.ops.yml" \
-  --vars-store creds.yml "$@"
+  --vars-store creds.yml \
+  --recreate
